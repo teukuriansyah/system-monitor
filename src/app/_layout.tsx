a40605643e103506671f08a047832a6d8f31d357
riansyah24 <jackrian72@gmail.com>
2026-09-21 01:04:19 +0700

    feat: add bottom bar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,15 @@
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
+import { Feather, Ionicons } from '@expo/vector-icons';
 import "../../global.css"
 import Navbar from "../components/Navbar"
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ header:() => <Navbar/>,}}/>
-      <Stack.Screen name="device" options={{ header:() => <Navbar/>}}/>
-      <Stack.Screen name="battery" options={{ header:() => <Navbar/>}}/>
-      <Stack.Screen name="settings" options={{ header:() => <Navbar/>}}/>
-    </Stack>
+    <Tabs>
+      <Tabs.Screen name="index" options={{ header:() => <Navbar/>, tabBarIcon:(focused) => <Feather name="home" size={20} color={focused?"blue":"black"}/>}}/>
+      <Tabs.Screen name="device" options={{ header:() => <Navbar/>}}/>
+      <Tabs.Screen name="battery" options={{ header:() => <Navbar/>}}/>
+      <Tabs.Screen name="settings" options={{ header:() => <Navbar/>}}/>
+    </Tabs>
   );
 }
