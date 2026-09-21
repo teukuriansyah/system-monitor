662bb45d23e92ab7d1853957a908865c098ab3a0
riansyah24 <jackrian72@gmail.com>
2026-09-21 11:55:42 +0700

    feat: add dashboard and settings

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,8 +4,64 @@ import { Link } from "expo-router"
 export default function Index() {
   return (
     <View>
-      <Link href="/device">Device</Link>
-      <Text>test</Text>
+      {/* Background Monitoring */}
+      <View className="px-5 py-2">
+        <View className="border rounded-lg p-3 gap-3">
+          <View>
+            <View></View>
+            <View>
+              <Text className="font-bold text-xl">Background Monitoring</Text>
+              <Text className="text-sm text-gray-400">Foreground Service Engine</Text>
+            </View>
+          </View>
+          <View className="flex flex-row justify-between">
+            <Text className="text-sm">Batt: </Text>
+            <Text className="text-sm">Temp: </Text>
+          </View>
+        </View>
+      </View>
+
+      {/* Device Summary */}
+      <View className="px-5 py-2">
+        <View className="flex gap-3 rounded-lg border p-4">
+          <View>
+            <Text className="font-bold text-xl">Device Summary</Text>
+          </View>
+          <View className="flex flex-row justify-between">
+            <View>
+              <Text className="text-sm text-gray-400">Model & Hardware</Text>
+              <Text className="font-bold">Model & Hardware</Text>
+            </View>
+            <View>
+              <Text className="text-sm text-gray-400">Model & Hardware</Text>
+              <Text className="font-bold">Model & Hardware</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
+      {/* Battery Telemetry */}
+      <View className="px-5 py-2">
+        <View className="border rounded-lg p-4">
+          <View>
+            <Text className="font-bold text-xl">Battery Telemetry</Text>
+          </View>
+          <View className="flex flex-row justify-between">
+            <View>
+              <Text>Temp</Text>
+              <Text>31°C</Text>
+            </View>
+            <View>
+              <Text>Temp</Text>
+              <Text>31°C</Text>
+            </View>
+            <View>
+              <Text>Temp</Text>
+              <Text>31°C</Text>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
--- src/app/settings.tsx
+++ src/app/settings.tsx
@@ -3,7 +3,22 @@ import { Text, View } from "react-native";
 export default function Battery() {
   return (
     <View>
-      <Text>test</Text>
+      <View className="px-5 py-2">
+        <View className="p-4 border rounded-xl">
+          <View>
+            <Text className="text-lg font-bold">Foreground Service</Text>
+          </View>
+          <View className="flex flex-row justify-between">
+            <View className="w-52">
+              <Text className="text-xl font-bold">Enable Background Monitoring</Text>
+              <Text className="text-sm">Runs Foreground Service with persistent status notification</Text>
+            </View>
+            <View>
+              <Text>Input</Text>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
